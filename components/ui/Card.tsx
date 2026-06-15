13329b1de6ca0687328eeaf0fa0c6ee0f96c0b0d
thermo-jam <nikamenskii@gmail.com>
2026-06-15 14:15:29 +0300

    feat(ui): атомы Button, Input, Card, EmptyState

---
+++ components/ui/Card.tsx
@@ -0,0 +1,12 @@
+import type { HTMLAttributes } from 'react';
+import { clsx } from 'clsx';
+
+type Props = HTMLAttributes<HTMLDivElement>;
+
+export function Card({ className, children, ...rest }: Props) {
+  return (
+    <div className={clsx('glass p-5', className)} {...rest}>
+      {children}
+    </div>
+  );
+}
